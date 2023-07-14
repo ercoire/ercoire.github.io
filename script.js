ccfe59574cb464d63d2d4b6d0aa4f8e22c2d1f78
Kasia Gierasimczuk <62510161+ercoire@users.noreply.github.com>
2023-07-14 20:47:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://replit.com/@ercoire/kasia-gierasimczuk-kodilla-tasks/v1/tasks';
+  var apiRoot = 'https://replit.com/@ercoire/kasia-gierasimczuk-kodilla-tasks/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
